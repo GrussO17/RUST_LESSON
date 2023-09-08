32080680a655005c145a9b0c6ec5e7ea51839fad
Owen Gruss <omg6429@rit.edu>
2023-09-08 11:02:29 -0400

    improve easy test

--- practice_easy/src/tests.rs
+++ practice_easy/src/tests.rs
@@ -1,15 +1,27 @@
 use super::count_above;
 
+fn reference_count_above(input: &Vec<i32>, min: i32) -> u32{ 
+    let mut ret: u32 = 0;
+    for val in input{
+        if *val >= min {
+            ret += 1;
+        }
+    }
+    return ret;
+}
+
 #[test]
 fn easy() {
     let list: Vec<i32> = vec![1, 2, 3, 4, 5, 6, 7, 8, 9];
     let ret = count_above(&list, 3);
-    assert!(ret == 7);
+    let answer = reference_count_above(&list, 3); 
+    assert!(ret == answer); // 7
 }
 #[test]
 fn hard() {
     let list: Vec<i32> = (-13..100).collect();
     let ret = count_above(&list, 3);
-    assert!(ret == 98);
+    let answer = reference_count_above(&list, 3);
+    assert!(ret == answer); // 97
 }
 
