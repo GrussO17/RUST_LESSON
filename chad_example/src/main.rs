8f828c02ddc911a5d09e0950c57c9192283bab23
Owen Gruss <omg6429@rit.edu>
2023-09-08 12:20:05 -0400

    example

--- chad_example/src/main.rs
+++ chad_example/src/main.rs
@@ -1,8 +1,12 @@
 fn main() {
+    ex1();   
+}
 
-    //This turns every word in a given sentance or string to the word "🍞".
-    let i = "this is a totally normal sentance.";
-    println!("Sentance Before: {}", i);
-
-    println!("Sentance after: {}",i.split(" ").map(|_x| "🍞 ").into_iter().collect::<String>());
+fn ex1() {
+     //This turns every word in a given sentance or string to the word "🍞".
+     let i = "this is a totally normal sentance.";
+     println!("Sentance Before: {}", i);
+ 
+    //THIS IS MY SINGLE LINE
+     println!("Sentance after: {}",i.split(" ").map(|_x| "🍞 ").into_iter().collect::<String>());
 }
